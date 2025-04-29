83aeae8badb4e0ddffbe95b93929352a970b3ad2
11 <1493229835@qq.com>
2025-04-29 11:28:29 +0800

    VOS

---
+++ VOS/data/aligned_dataset.py
@@ -0,0 +1,298 @@
+import os.path
+import random
+
+import numpy
+import torchvision.transforms as transforms
+import torch
+from .base_dataset import BaseDataset
+from .image_folder import make_dataset
+from PIL import Image
+
+import cv2
+import skimage
+from skimage import data, exposure, img_as_float,io
+import numpy as np
+from util import util
+
+class AlignedDataset(BaseDataset):  # 继承BaseDataset父类，实现将图像进行编号分类
+    def one(self):
+        # 初始化空数组
+        AlignedDataset.l1_results = np.empty(0,dtype=int)
+        AlignedDataset.l2_results = np.empty(0,dtype=int)   # 保存总的序列
+        AlignedDataset.l1_results_3 = np.empty(0,dtype=int)  # 积攒的数值
+        AlignedDataset.l2_results_3 = np.empty(0,dtype=int)
+
+    def initialize(self, opt, line = 0, epoch =1,two = 1):
+        self.opt = opt
+        # self.root = opt.dataroot
+        self.root = ''
+        self.dir_AB = os.path.join(opt.dataroot, opt.phase) # 获得图像地址
+        self.AB_paths = sorted(make_dataset(self.dir_AB)) # 返回图像的路径，读取的是一张图像
+        assert(opt.resize_or_crop == 'resize_and_crop') # 检查命令是否是需要的
+        self.two = two
+        # --------------------调试-------------------------------------
+        AlignedDataset.l1_results = np.empty(0, dtype=int)
+        AlignedDataset.l2_results = np.empty(0, dtype=int)  # 保存总的序列
+        AlignedDataset.l1_results_3 = [212]
+        AlignedDataset.l2_results_3 = [320]
+        AlignedDataset.line_l1 = 0
+        AlignedDataset.line_l2 = 0
+        AlignedDataset.d_ls = 0
+        AlignedDataset.best_ls = 0
+        AlignedDataset.best_ls_x = 0
+        print('输入l1：',AlignedDataset.line_l1)
+        print('输入l2：', AlignedDataset.line_l2)
+        # ------------------------------------------------------------
+        print('line',line)
+
+        if two == 1 and len(AlignedDataset.l1_results_3)==0:
+
+            if line > 0:    # 首先判断是否有偏移量,大于0向右移动
+                # --------------计算最优滑动距离--------------------------------
+                AlignedDataset.best_ls,AlignedDataset.best_ls_x = self.huadong_ls(line)
+                # --------------计算最优滑动距离--------------------------------
+                AlignedDataset.line_l1 = AlignedDataset.line_l1 + AlignedDataset.best_ls
+                AlignedDataset.line_l2 = 108 + AlignedDataset.line_l1
+                AlignedDataset.d_ls = 1
+            elif line < 0:
+                # --------------计算最优滑动距离--------------------------------
+                AlignedDataset.best_ls,AlignedDataset.best_ls_x= self.huadong_ls(line)
+                # --------------计算最优滑动距离--------------------------------
+                AlignedDataset.line_l1 = AlignedDataset.line_l1 - AlignedDataset.best_ls
+                AlignedDataset.line_l2 = 108 + AlignedDataset.line_l1
+                AlignedDataset.d_ls = -1
+            else:
+                if epoch != 1:   # 如果是不是第一次输入则执行上次的累加结果
+                    AlignedDataset.line_l1 = AlignedDataset.line_l1
+                    AlignedDataset.line_l2 = 108 + AlignedDataset.line_l1
+                    AlignedDataset.d_ls = 0
+                    AlignedDataset.best_ls = 0
+                    AlignedDataset.best_ls_x =0
+                else:       # 如果是第一次输入则执行初始化
+                    AlignedDataset.line_l1 = int(self.opt.loadSize_w / 3)
+                    AlignedDataset.line_l2 = 108 + AlignedDataset.line_l1
+                    AlignedDataset.d_ls = 0
+                    AlignedDataset.best_ls = 0
+                    AlignedDataset.best_ls_x = 0
+            # 闸值限制
+            if AlignedDataset.line_l1 < 0:
+                AlignedDataset.line_l1 = 0
+            elif AlignedDataset.line_l1 > 212:
+                AlignedDataset.line_l1 = 212
+            else:
+                AlignedDataset.line_l1 = AlignedDataset.line_l1
+
+            if AlignedDataset.line_l2 < 108:
+                AlignedDataset.line_l2 = 108
+            elif AlignedDataset.line_l2 > 320:
+                AlignedDataset.line_l2 = 320
+            else:
+                AlignedDataset.line_l2 = AlignedDataset.line_l2
+
+            AlignedDataset.l1_results = np.append(AlignedDataset.l1_results, AlignedDataset.line_l1)
+            AlignedDataset.l2_results = np.append(AlignedDataset.l2_results, AlignedDataset.line_l2)
+            if AlignedDataset.l1_results.size <= 1:
+                AlignedDataset.l1_results_3 = AlignedDataset.l1_results.copy()  # 如果元素个数少于等于3，则赋值为results
+                AlignedDataset.l2_results_3 = AlignedDataset.l2_results.copy()  # 如果元素个数少于等于3，则赋值为results
+            else:
+                AlignedDataset.l1_results_3 = AlignedDataset.l1_results[-1:]  # 如果元素个数大于3，则赋值为results的最新三个值
+                AlignedDataset.l2_results_3 = AlignedDataset.l2_results[-1:]  # 如果元素个数大于3，则赋值为results的最新三个值
+
+            self.line_l1 = AlignedDataset.l1_results_3[-1]
+            self.line_l2 = AlignedDataset.l2_results_3[-1]
+
+
+        elif two == 1 and len(AlignedDataset.l1_results_3):
+            self.line_l1 = AlignedDataset.l1_results_3[-1]
+            self.line_l2 = AlignedDataset.l2_results_3[-1]
+
+        elif two == 2:
+            self.line_l1 = AlignedDataset.l1_results_3[-1]
+            self.line_l2 = AlignedDataset.l2_results_3[-1]
+            AlignedDataset.l1_results_3 = np.delete(AlignedDataset.l1_results_3, -1)
+            AlignedDataset.l2_results_3 = np.delete(AlignedDataset.l2_results_3, -1)
+            AlignedDataset.d_ls = AlignedDataset.d_ls
+            AlignedDataset.best_ls = AlignedDataset.best_ls
+            AlignedDataset.best_ls_x = AlignedDataset.best_ls_x
+
+        print('self.l1_results', AlignedDataset.l1_results)
+        print('self.l2_results', AlignedDataset.l2_results)
+
+        print('self.l1_results_3',AlignedDataset.l1_results_3)
+        print('self.l2_results_3', AlignedDataset.l2_results_3)
+
+        print('输出l1：', self.line_l1)
+        print('输出l2：', self.line_l2)
+
+        return self.line_l1, self.line_l2, AlignedDataset.best_ls_x, AlignedDataset.d_ls
+
+    def __getitem__(self, index):
+
+
+        AB_path = self.AB_paths[index]    # 读取图片
+
+        AB = Image.open(AB_path).convert('RGB')
+        w, h = AB.size
+        w2 = int(w / 2)
+        A = AB.crop((0, 0, w2, h)).resize((self.opt.loadSize_w, self.opt.loadSize_h), Image.BICUBIC)
+        B = AB.crop((w2, 0, w, h)).resize((self.opt.loadSize_w, self.opt.loadSize_h), Image.BICUBIC)
+
+        # 裁剪A得到a,b
+        A_w,A_h = A.size
+        # l1 = int((A_w+1)/3)
+        # l2 = int(A_w -l1)
+
+
+        l1 = self.line_l1
+        l2 = self.line_l2
+
+
+        a = A.crop((0, 0, l2, A_h)).resize((256, 256),Image.BICUBIC)
+        b = A.crop((l1, 0, A_w, A_h),).resize((256, 256),Image.BICUBIC)
+        Ba = B.crop((0, 0, l2, A_h)).resize((256, 256),Image.BICUBIC)
+        Bb = B.crop((l1, 0, A_w, A_h)).resize((256, 256),Image.BICUBIC)
+        Aa = A.crop((0, 0, l2, A_h)).resize((256, 256),Image.BICUBIC)          # 红外A中的a
+        Ab = A.crop((l1, 0, A_w, A_h)).resize((256, 256),Image.BICUBIC)        # 红外A中的b
+
+
+        # 将 a,b,Aa，Ba转换为array类型
+        a = np.array(a)
+        b = np.array(b)
+        Ba = np.array(Ba)
+        Bb = np.array(Bb)
+        A = np.array(A)
+        B = np.array(B)
+        Aa = np.array(Aa)
+        Ab = np.array(Ab)
+
+
+
+        a = Image.fromarray(np.uint8(a)).convert('RGB')  # 原始的 NumPy 数组 A 被转换为 PIL 的 Image 对象,并且保证了数据类型和色彩空间的正确性。
+        b = Image.fromarray(np.uint8(b)).convert('RGB')
+        Ba = Image.fromarray(np.uint8(Ba)).convert('RGB')  # 原始的 NumPy 数组 A 被转换为 PIL 的 Image 对象,并且保证了数据类型和色彩空间的正确性。
+        Bb = Image.fromarray(np.uint8(Bb)).convert('RGB')
+        A = Image.fromarray(np.uint8(A)).convert('RGB')  # 原始的 NumPy 数组 A 被转换为 PIL 的 Image 对象,并且保证了数据类型和色彩空间的正确性。
+        B = Image.fromarray(np.uint8(B)).convert('RGB')
+        Aa = Image.fromarray(np.uint8(Aa)).convert('RGB')  # 原始的 NumPy 数组 A 被转换为 PIL 的 Image 对象,并且保证了数据类型和色彩空间的正确性。
+        Ab = Image.fromarray(np.uint8(Ab)).convert('RGB')
+
+
+
+        a = transforms.ToTensor()(a)  # (0 - 255) --> (0 - 1)
+        b = transforms.ToTensor()(b)
+        Ba = transforms.ToTensor()(Ba)  # (0 - 255) --> (0 - 1)
+        Bb = transforms.ToTensor()(Bb)
+        A = transforms.ToTensor()(A)  # (0 - 255) --> (0 - 1)
+        B = transforms.ToTensor()(B)
+        Aa = transforms.ToTensor()(Aa)  # (0 - 255) --> (0 - 1)
+        Ab = transforms.ToTensor()(Ab)
+
+
+        # img_a =  a.unsqueeze(0)  # 给矢量数组增添一个维度
+        # img_b =  b.unsqueeze(0)  # 给矢量数组增添一个维度
+        # img_a = util.tensor2im(img_a)
+        # img_b = util.tensor2im(img_b)
+        #
+        # cv2.imshow('1', img_a)  # 生成的a
+        # cv2.imshow('2', img_b)  # 生成的b
+        #
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+
+        a = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(a)
+        b = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(b)
+        Ba = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(Ba)
+        Bb = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(Bb)
+        A = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(A)
+        B = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(B)
+        Aa = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(Aa)
+        Ab = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(Ab)
+
+        if self.opt.which_direction == 'BtoA':
+            input_nc = self.opt.output_nc
+            output_nc = self.opt.input_nc
+        else:
+            input_nc = self.opt.input_nc
+            output_nc = self.opt.output_nc
+
+
+        if input_nc == 1:  # RGB to gray
+            tmp_a = a[0, ...] * 0.299 + a[1, ...] * 0.587 + a[2, ...] * 0.114
+            a = tmp_a.unsqueeze(0)
+            tmp_b = b[0, ...] * 0.299 + b[1, ...] * 0.587 + b[2, ...] * 0.114
+            b = tmp_b.unsqueeze(0)
+            tmp_A = A[0, ...] * 0.299 + A[1, ...] * 0.587 + A[2, ...] * 0.114
+            A = tmp_A.unsqueeze(0)
+            tmp_Aa = Aa[0, ...] * 0.299 + Aa[1, ...] * 0.587 + Aa[2, ...] * 0.114
+            Aa = tmp_Aa.unsqueeze(0)
+            tmp_Ab = Ab[0, ...] * 0.299 + Ab[1, ...] * 0.587 + Ab[2, ...] * 0.114
+            Ab = tmp_Ab.unsqueeze(0)
+
+
+
+        if output_nc == 1:  # RGB to gray
+            tmp_Ba = Ba[0, ...] * 0.299 + Ba[1, ...] * 0.587 + Ba[2, ...] * 0.114
+            Ba = tmp_Ba.unsqueeze(0)
+            tmp_Bb = Bb[0, ...] * 0.299 + Bb[1, ...] * 0.587 + Bb[2, ...] * 0.114
+            Bb = tmp_Bb.unsqueeze(0)
+            tmp_B = Bb[0, ...] * 0.299 + Bb[1, ...] * 0.587 + Bb[2, ...] * 0.114
+            B = tmp_B.unsqueeze(0)
+
+
+        # print('a.shape:',a.shape)
+        # print('b.shape:',b.shape)
+        # print('Ba.shape:',Ba.shape)
+        # print('Bb.shape:',Bb.shape)
+        # print('A.shape:',A.shape)
+        # print('B.shape:',B.shape)
+        # print('Aa.shape:', Aa.shape)
+        # print('Ab.shape:', Ab.shape)
+
+        return {'a': a, 'b': b,'Ba': Ba, 'Bb': Bb,'A':A,'B':B,'Aa':Aa,'Ab':Ab,
+                'a_paths': AB_path, 'b_paths': AB_path,'Ba_paths': AB_path, 'Bb_paths': AB_path,'A_paths': AB_path, 'B_paths': AB_path}
+
+
+
+
+    def __len__(self):
+        return len(self.AB_paths)
+
+    def name(self):
+        return 'AlignedDataset'
+
+    def huadong_ls(self,line,ls_min=0, ls_max=10, x_min=-5, x_max=5, channel=256):
+        # 初始化最小绝对值和对应的ls, x
+        Xl_l1 = self.line_l1
+        Xl_l2 = self.line_l2
+        min_e_abs = float('inf')
+        best_ls = None
+        best_x = None
+        # 遍历可能的ls和x的取值
+        if line != 0:
+            for ls in range(ls_min, ls_max):  # 假设ls的范围
+                for x in range(x_min, x_max):  # 假设x的范围
+                    # for Xl in Xl_range:
+                    # 计算Y和e
+                    if line > 0 :
+                        Y = ls * (ls + Xl_l2) / channel
+                        e = ls + x - Y
+                    elif line < 0 :
+                        Y = ls * (320 + ls - Xl_l1) / channel
+                        e = ls + x - Y
+                    elif ls + x <= 0:
+                        e =0
+
+                    # 检查当前e的绝对值是否为最小
+                    if abs(e) < min_e_abs and e != 0:
+                        min_e_abs = abs(e)
+                        best_ls = ls
+                        best_x = x
+        else:
+            best_ls = 0
+            best_x = 0
+        # 打印结果
+        best_ls_x = best_ls + best_x
+        print(f"最小绝对值的e: {min_e_abs}, 对应的ls: {best_ls}, x: {best_x},ls_x:{best_ls_x}")
+        return best_ls,best_ls_x
---
+++ VOS/train.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+import time
+
+import cv2
+import numpy as np
+
+from options.train_options import TrainOptions
+from data import CreateDataLoader
+from models import create_model
+from util.visualizer import Visualizer
+from models.pix2pix_model import Pix2PixModel
+from models.CDGAN_model import CDGANModel
+from options.test_options import TestOptions
+import math
+import csv
+import ntpath
+import os
+from util import util
+from tqdm import tqdm
+from util.visualizer import save_images
+from util import html
+import evaluate
+
+#  python train.py --which_epoch 0
+
+'''训练时间长的原因分析
+    
+   ①训练的输出太多，正常应该每100张图片输出一次
+   ②
+
+'''
+
+if __name__ == '__main__':
+    opt = TrainOptions().parse()
+    opt2 = TestOptions().parse()
+
+    model = create_model(opt)
+    model.setup(opt) # 决定模型的优化器
+    visualizer = Visualizer(opt)
+    total_steps = 0
+    total_iters = 0  # the total number of training iterations
+    line = 0
+    Epoch_s = 0
+
+
+    # 输出测试的图片
+    if os.path.exists(opt.train_path) is not True:
+        os.makedirs(opt.train_path)
+    if os.path.exists(opt.train_path) is not True:
+        os.makedirs(opt.test_path)
+    # if os.path.exists(r'D:\GAN-NET\MUGAN-me\test\test\attion') is not True:
+    #     os.makedirs(r'D:\GAN-NET\MUGAN-me\test\test\attion')
+
+    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+        epoch_start_time = time.time()
+        iter_data_time = time.time()
+        epoch_iter = 0
+        t_data = 0
+
+        # 初始化让评价指标清零
+        ssim_sum_a = 0.0
+        psnr_sum_a = 0.0
+        ssim_sum_b = 0.0
+        psnr_sum_b = 0.0
+
+        # 加载数据
+        data_loader,l1,l2,best_ls_x,d_ls = CreateDataLoader(opt,line,epoch)
+        dataset = data_loader.load_data()
+        data_loader_test,l1,l2,best_ls_x,d_ls = CreateDataLoader(opt2,line,epoch,two=2)  # 第二次输入不改变尺寸
+        dataset_test = data_loader_test.load_data()
+
+        line = 0  # 将之前的偏移量进行清0
+
+        dataset_size = len(data_loader)
+        dataset_size_test = len(data_loader_test)
+        print('#training images = %d' % dataset_size)
+
+        for i, data in enumerate(dataset):
+
+            psnr_a = 0.0
+            ssim_a = 0.0
+            psnr_b = 0.0
+            ssim_b = 0.0
+
+            iter_start_time = time.time()
+            if total_steps % opt.print_freq == 0:   #  opt.print_freq = 100  100 次更新一次时间信息
+                if t_data:
+                    next_data = (iter_start_time - t_data-iter_data_time) / 100 * dataset_size
+                else:
+                    next_data = (iter_start_time - iter_data_time) / 100 * dataset_size
+                t_data = iter_start_time - iter_data_time
+            visualizer.reset()
+            total_steps += opt.batchSize
+            epoch_iter += opt.batchSize
+            model.set_input(data)
+            line = model.optimize_parameters(l1,l2,line,best_ls_x,d_ls)
+            # 返回损失
+            psnr_a,ssim_a,psnr_b,ssim_b = model.return_PSNR_SSIM()
+            # 得到总损失
+            psnr_sum_a = psnr_a + psnr_sum_a
+            ssim_sum_a = ssim_a + ssim_sum_a
+            ssim_sum_b = ssim_b + ssim_sum_b
+            psnr_sum_b = psnr_b + psnr_sum_b
+
+            total_iters += opt.batchSize
+            losses = model.get_current_losses()  # ！！！
+
+            if total_iters % opt.display_freq == 0:  # display images on visdom and save images to a HTML file   opt.display_freq = 100
+                save_result = total_iters % opt.update_html_freq == 0 #  opt.update_html_freq = 100   100轮结果更新一次图像
+                model.compute_visuals()
+
+                # -----------------得到原名-----------------------------------
+                img_path = model.get_image_paths()
+                short_path = ntpath.basename(img_path[0])
+                name = os.path.splitext(short_path)[0]
+                # # -----------------得到原名-----------------------------------
+                # visuals = model.get_current_visuals()
+                # for label, image in visuals.items():
+                #     image_numpy = util.tensor2im(image)
+                #     util.save_image(image_numpy,
+                #                 r'D:\GAN-NET\MUGAN-me\test\test\attion/' + '%s_%s_%d_%d.png' % (
+                #                     label , name, total_iters, 3))
+
+                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result,name)
+
+
+            # if total_steps % opt.display_freq == 0:   # opt.display_freq = 100
+            #     save_result = total_steps % opt.update_html_freq == 0
+            #     # -----------------得到原名-----------------------------------
+            #     img_path = model.get_image_paths()
+            #     short_path = ntpath.basename(img_path[0])
+            #     name = os.path.splitext(short_path)[0]
+            #     # -----------------得到原名-----------------------------------
+            #     visualizer.display_current_results(model.get_current_visuals(), epoch, save_result, name)
+
+            if total_steps % opt.print_freq == 0:   # opt.print_freq = 100 100轮输出结果
+                #losses = model.get_current_losses()
+                t = (time.time() - iter_start_time) / opt.batchSize
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data,next_data)
+                if opt.display_id > 0:
+                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+
+
+                    # -----------------------------修改--------------------------------------------
+                    # -----------------------------打印损失信息-------------------------------------
+                    message_loss = ''
+                    for k, v in losses.items():
+                        message_loss += '%s:%.3f,' % (k, v)
+                    f1 = open(r'./each_loss.csv', 'a', newline='')
+
+                    csv_writer = csv.writer(f1)
+                    csv_writer.writerow(['epoch:%d,'%epoch+'number:%d,'%i+message_loss])
+                    f1.close()
+                    # -----------------------------修改--------------------------------------------
+
+                    #  ----------------------------打印尺寸 ----------------------------------------
+
+                    f2 = open(r'./l1l2.csv', 'a', newline='')
+
+                    csv_writer = csv.writer(f2)
+                    csv_writer.writerow(['epoch:%d,' % epoch + 'l1:%d,' % l1 + 'l2:%d,' % l2 + 'line:%d,' % line])
+                    f2.close()
+                    #  ----------------------------打印尺寸 ----------------------------------------
+
+                    #  ----------------------------打印PSNR，SSIM损失 ----------------------------------------
+
+                    f3 = open(r'./PSNR_SSIM.csv', 'a', newline='')
+
+                    csv_writer = csv.writer(f3)
+                    csv_writer.writerow(['psnr_a:%.4f \t ssim_a:%.4f \t psnr_b:%.4f \t ssim_b:%.4f \t Apsnr_sum_a:%.4f \t Assim_sum_a:%.4f \t Apsnr_sum_b:%.4f'
+                  ' \t Assim_sum_b:%.4f \t'%(psnr_a,ssim_a,psnr_b,ssim_b,psnr_sum_a/epoch_iter, ssim_sum_a/epoch_iter,
+                                          psnr_sum_b/epoch_iter, ssim_sum_b/epoch_iter)])
+                    f3.close()
+                    #  ----------------------------打印尺寸 ----------------------------------------
+
+            # 打印每一小轮的训练时间进行观察
+            print('epoch:%d \t End of epoch %d / %d \t Time Taken: %.3f sec\t  Time Total: %.3f sec' %
+                  (epoch, dataset_size, epoch_iter, (time.time() - iter_start_time) / opt.batchSize,
+                   (time.time() - epoch_start_time) / opt.batchSize))
+
+            print('psnr_a:%.4f \t ssim_a:%.4f \t psnr_b:%.4f \t ssim_b:%.4f \t Apsnr_sum_a:%.4f \t Assim_sum_a:%.4f \t Apsnr_sum_b:%.4f'
+                  ' \t Assim_sum_b:%.4f \t'%(psnr_a,ssim_a,psnr_b,ssim_b,psnr_sum_a/epoch_iter, ssim_sum_a/epoch_iter,
+                                          psnr_sum_b/epoch_iter, ssim_sum_b/epoch_iter))
+
+
+        if epoch == opt.niter + opt.niter_decay:   # 判断是否是最后一次训练
+            print('saving the latest model (epoch %d, total_steps %d)' %
+                  (epoch, total_steps))
+
+            model.save_networks('latest')
+
+            iter_data_time = time.time()
+
+        if epoch <= opt.niter + opt.niter_decay and epoch % opt.train_number == 0:
+            print('saving the model at the end of epoch %d, iters %d' %
+                  (epoch, total_steps))
+            model.save_networks(epoch)
+
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+              (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+
+        model.update_learning_rate()
+
+        # # 将模型进行预测
+        # if epoch % 1 == 0:  # 限制epoch为整数
+        #         epoch_start_time = time.time()
+        #         iter_data_time = time.time()
+        #         epoch_iter = 0
+        #
+        #         # create website
+        #         web_dir = os.path.join(opt2.results_dir, opt2.name, '%s_%s' % (opt2.phase, epoch))
+        #         print('web_dir',web_dir)
+        #         webpage = html.HTML(web_dir,
+        #                             'Experiment = %s, Phase = %s, Epoch = %s' % (opt2.name, opt2.phase, epoch))
+        #         for i, data in enumerate(dataset_test):
+        #             # if i > 1000 and epoch < 15:
+        #             #     break
+        #             model.set_input(data)
+        #             model.test(l1,l2)
+        #
+        #             visuals = model.get_current_visuals()
+        #             img_path = model.get_image_paths()
+        #
+        #             print('processing (%04d)-th image... %s' % (i, img_path))
+        #             save_images(webpage, visuals, img_path, aspect_ratio=opt2.aspect_ratio, width=opt.display_winsize)
+        #
+        #         # 将预测的模型进行预测
+        #         test_path = web_dir +'//images'
+        #         print('test_path',test_path)
+        #         test_psnr_a,test_psnr_b,test_ssim_a,test_ssim_b,test_lpips_a,test_lpips_b = evaluate.PSNR_SSIM_LPIPS(test_path)
+        #         print('Average: avpsnr_a={} avpsnr_b={} avssim_a={} avssim_b={} avlpips_a{} avlpips_b{}\n'.format(test_psnr_a, test_psnr_b,test_ssim_a,test_ssim_b, test_lpips_a,test_lpips_b))
+        #
+        #         #  ----------------------------保存结果 ----------------------------------------
+        #
+        #         f3 = open(r'D:\GAN-NET\MUGAN-me\test\AVPSNR_AVSSIM_AVLPIPS.csv', 'a', newline='')
+        #
+        #         csv_writer = csv.writer(f3)
+        #         csv_writer.writerow(['Apsnr_a:%.4f \t Apsnr_b:%.4f \t Assim_a:%.4f \t Assim_b:%.4f \t Alpips_a:%.4f \t Alpips_b:%.4f \t '
+        #                              %(test_psnr_a, test_psnr_b,test_ssim_a,test_ssim_b, test_lpips_a,test_lpips_b)])
+        #         f3.close()
+        #         #  ----------------------------保存结果 ----------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
